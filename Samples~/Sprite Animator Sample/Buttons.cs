d7f8c37e89279c631c2dfa06a0bff27d1631f3fe
bbischoff <benjamin.bischoff@trivago.com>
2020-11-25 12:57:03 +0100

    trying sample

---
+++ Samples~/Sprite Animator Sample/Buttons.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour
+{
+    public enum ButtonAction { RESET, PAUSE, SWITCH };
+    public static event Action<ButtonAction> OnPress;
+
+    public void ResetButton()
+    {
+        OnPress?.Invoke(ButtonAction.RESET);
+    }
+
+    public void PauseButton()
+    {
+        OnPress?.Invoke(ButtonAction.PAUSE);
+    }
+
+    public void SwitchButton()
+    {
+        OnPress?.Invoke(ButtonAction.SWITCH);
+    }
+}
---
+++ Samples~/Sprite Animator Sample/Example.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using blog.softwaretester.spriteanimator;
+
+public class Example : MonoBehaviour
+{
+    private SpriteAnimator spriteAnimator;
+    private enum SpriteGroup { cube, thing };
+
+    [SerializeField]
+    private SpriteAnimator.AnimationMode animationMode;
+
+    private bool isPaused;
+    private SpriteGroup currentGroup;
+    private TMPro.TMP_Text modeLabel;
+    private TMPro.TMP_Text triggerLabel;
+
+    private void Start()
+    {
+        TMPro.TMP_Text[] textFields = GetComponentsInChildren<TMPro.TMP_Text>();
+        modeLabel = textFields[0];
+        triggerLabel = textFields[1];
+        triggerLabel.text = "";
+
+        modeLabel.text = animationMode.ToString();
+
+        spriteAnimator = GetComponentInChildren<SpriteAnimator>();
+        spriteAnimator.OnTrigger += OnTrigger;
+        currentGroup = SpriteGroup.thing;
+        spriteAnimator.Play(currentGroup.ToString(), animationMode);
+
+        Buttons.OnPress += OnPress;
+    }
+
+    private void OnTrigger(int frame, string triggerName)
+    {
+        triggerLabel.text = triggerName;
+    }
+
+    private void OnPress(Buttons.ButtonAction action)
+    {
+        switch (action)
+        {
+            case Buttons.ButtonAction.RESET:
+                spriteAnimator.Play(currentGroup.ToString(), animationMode);
+                break;
+            case Buttons.ButtonAction.PAUSE:
+                isPaused = !isPaused;
+                if (isPaused)
+                {
+                    spriteAnimator.Pause();
+                }
+                else
+                {
+                    spriteAnimator.Resume();
+                }
+                break;
+            case Buttons.ButtonAction.SWITCH:
+                currentGroup = currentGroup == SpriteGroup.cube ? SpriteGroup.thing : SpriteGroup.cube;
+                spriteAnimator.Play(currentGroup.ToString(), animationMode);
+                isPaused = false;
+                triggerLabel.text = "";
+                break;
+        }
+    }
+}
